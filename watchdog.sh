1aa59d12f2f3e03a0e124e3604dec11845d96b23
Martin Kribs <54333035+martinkribs@users.noreply.github.com>
2022-01-04 16:35:20 +0100

    easier handling for users in unpacking

--- watchdog.sh
+++ watchdog.sh
@@ -1,4 +1,4 @@
 
 #!/bin/sh
-/bin/python3 /home/pi/serialReadAllOutput.py &
-echo "$(date +'%T')-CO2 Script was started with the pi" >> /home/pi/log.txt
+/bin/python3 /home/pi/co2-lights-main/serialReadAllOutput.py &
+echo "$(date +'%T')-CO2 Script was started with the pi" >> /home/pi/co2-lights-main/log.txt
