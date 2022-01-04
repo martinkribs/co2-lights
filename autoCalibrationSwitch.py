45801a1133b79743ea9093132ac94b2c1b60a8b8
Martin Kribs <54333035+martinkribs@users.noreply.github.com>
2022-01-04 20:11:01 +0100

    autoCalibration added

---
+++ autoCalibrationSwitch.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+import serial
+
+ser = serial.Serial(
+        port='/dev/ttyS0',
+        baudrate = 9600,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=5
+)
+
+command = bytearray([0xff, 0x01, 0x79, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79])
+ser.write(command)
+
+file = open("autoCalibration.conf", "w+")
+value=file.read
+if (value=="1"):
+    print("Auto Calibration wurde deaktiviert")
+    file.write("0")
+else:
+    print("Auto Calibration wurde aktiviert")
+    file.write("1")
+file.close()
